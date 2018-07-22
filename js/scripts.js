1544f116fcc1530882db3e4939005bf8221f4938
Charles Seymour <charlesseymour@yahoo.com>
2018-07-22 12:20:56 -0500

    Changed transcript click function for IE compatibility

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 const spans = document.getElementsByTagName('span');
 const vid = document.getElementsByTagName('video')[0];
 
-vid.addEventListener('timeupdate', () => {
+vid.addEventListener('timeupdate', function(){
   for (let i = 12; i < spans.length; i++) {
     if (vid.currentTime >= spans[i].dataset.start && vid.currentTime <= spans[i].dataset.end) {
       spans[i].style.color = "red";
@@ -11,11 +11,28 @@ vid.addEventListener('timeupdate', () => {
   }
 });
 
-for (let i = 12; i < spans.length; i++) {
-  spans[i].addEventListener('click', () => {
-    vid.currentTime = spans[i].dataset.start;  
-  });
-};
+console.log(spans[13]);
 
+// Works in Chrome and Firefox but not IE:
+
+/*for (let i = 0; i < spans.length; i++) {
+  if (spans[i].hasAttribute('data-start')) {
+      spans[i].addEventListener('click', function() {
+        vid.currentTime = spans[i].dataset.start;
+      });
+  }; 
+};*/
+
+
+// Works in Chrome, Firefox, and IE:
+
+for (let i = 0; i < spans.length; i++) {
+  if (spans[i].hasAttribute('data-start')) {
+      (function(x) {
+        spans[x].addEventListener('click', function() {
+          vid.currentTime = spans[x].dataset.start;
+      })})(i);
+  }; 
+};
  
  
